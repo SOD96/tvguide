9f58413239f6ed2fe042f52f628a6bc27b0865aa
Sean <xengaming@outlook.com>
2015-01-31 00:49:22 +0000

    more updates to my application

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,20 +1,13 @@
 <?php
 
-class HomeController extends BaseController {
+class HomeController extends BaseController
+{
 
-	/*
-	|--------------------------------------------------------------------------
-	| Default Home Controller
-	|--------------------------------------------------------------------------
-	|
-	| You may wish to use controllers instead of, or in addition to, Closure
-	| based routes. That's great! Here is an example controller method to
-	| get you started. To route to this controller, just add the route:
-	|
-	|	Route::get('/', 'HomeController@showWelcome');
-	|
-	*/
-
+	/**
+	 * Shows the welcome page.
+	 * 
+	 * @return View
+	 */
 	public function showWelcome()
 	{
 		return View::make('hello');
